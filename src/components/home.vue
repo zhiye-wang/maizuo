e7531a0126d043ba610029bb88b99bea125fa9b3
wangzhiye <542647875@qq.com>
2018-11-21 15:16:01 +0800

    数据请求成功

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,7 +1,12 @@
 <template>
   <div >
    	home
-   
+   	<ul>
+   		<li v-for="data in datalist">
+   			<img :src="data.pic_url" alt="">
+   			<h2>{{data.title}}</h2>
+   		</li>
+   	</ul>
   </div>
 </template>
 
@@ -9,15 +14,17 @@
 	import axios from "axios"
 export default {
   name: 'home',
-  data () {
-    return {
-     
-    }
+  data(){
+  	return {
+  		 datalist:[]
+  	}
+   
   },
 
   mounted(){		
   	axios.get("/api/getGoods?page=1&zy_ids=p8_c4_l4_0&app_name=zhe&catname=tab_hpzc&flag=tab_hpzc").then(res=>{
-  		console.log(res)
+  		console.log(res.data)
+  		this.datalist = res.data.data.goods
   	}).catch(error=>{
   		console.log(error)
   	})
@@ -27,6 +34,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
+	img{
+		width:100%;
+		
+	}
 
 </style>
